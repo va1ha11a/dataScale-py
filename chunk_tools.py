45d7a0f4a2bc9b7fa0d5ec0d1b9b0ee80e4cb3fa
MRORANGE\valhalla <robbie@merge42.com>
2019-05-16 18:32:33 +1000

    Tidying up and moving stuff

---
+++ chunk_tools.py
@@ -0,0 +1,21 @@
+import hashlib
+from chunk_store import get_or_create_bucket, put_chunk, get_chunk
+
+def hash_chunk(chunk):
+    m = hashlib.sha256()
+    m.update(chunk)
+    return m.hexdigest()
+
+def get_chunks(file_name):
+    with open(file_name, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
+            yield chunk
+
+def store_chunk(chunk_data, chunk_hash, chunk_len, bucket_loc):
+    bucket = chunk_hash[:8]
+    get_or_create_bucket(bucket, bucket_loc)
+    put_chunk(bucket, chunk_hash, chunk_data, chunk_len)
+
+def restore_chunk(chunk_hash):
+    bucket = chunk_hash[:8]
+    return get_chunk(bucket, chunk_hash)
--- main.py
+++ main.py
@@ -1,29 +1,8 @@
-import hashlib
-
-from meta import create_file_meta, create_chunk_node, redis_graph
-from block import get_or_create_bucket, put_chunk, get_chunk
 import settings as st
 
+from meta import create_file_meta, create_chunk_node, redis_graph
+from chunk_tools import hash_chunk, get_chunks, store_chunk, restore_chunk
 from redisgraph import Node, Edge, Graph
-
-def hash_chunk(chunk):
-    m = hashlib.sha256()
-    m.update(chunk)
-    return m.hexdigest()
-
-def get_chunks(file_name):
-    with open(file_name, 'rb') as f:
-        for chunk in iter(lambda: f.read(4096), b''):
-            yield chunk
-
-def store_chunk(chunk_data, chunk_hash, chunk_len, bucket_loc=st.bucket_location):
-    bucket = chunk_hash[:8]
-    get_or_create_bucket(bucket, bucket_loc)
-    put_chunk(bucket, chunk_hash, chunk_data, chunk_len)
-
-def restore_chunk(chunk_hash):
-    bucket = chunk_hash[:8]
-    return get_chunk(bucket, chunk_hash)
 
 if __name__ == "__main__":
     source_file_name = st.source_file_name
@@ -35,7 +14,7 @@ if __name__ == "__main__":
     for order, cnk in enumerate(cnks):
         hsh = hash_chunk(cnk)
         cnk_len = len(cnk)
-        store_chunk(cnk, hsh, cnk_len)
+        store_chunk(cnk, hsh, cnk_len, st.bucket_location)
         create_chunk_node(hsh, order, cnk_len, meta_file_obj)
 
     redis_graph.commit()
